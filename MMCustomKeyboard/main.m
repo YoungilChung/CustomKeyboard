972e8d71e197339eb7c0b54461c23fc5216d6b95
mm0030240 <mm0030240@monkadminsmini3.hil1.nl.client.mediamonks.local>
2015-10-06 09:25:09 +0200

    Initial Commit

---
+++ MMCustomKeyboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MMCustomKeyboard
+//
+//  Created by mm0030240 on 06/10/15.
+//  Copyright © 2015 mm0030240. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
